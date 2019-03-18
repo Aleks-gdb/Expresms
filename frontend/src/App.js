3d0e39a62c96767bbee68b729ad904472c8d41fd
Aleks <jokerka17@gmail.com>
2019-03-17 18:45:09 -0700

    Color change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import Navigation from './Navigation';
 import './App.css';
 
@@ -7,22 +6,8 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="mainnavbar">
       <Navigation />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
       </div>
     );
   }
