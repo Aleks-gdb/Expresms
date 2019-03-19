e6e5d3166c0a29b9cbc0a8c611d8b64a77bb229d
Aleks <jokerka17@gmail.com>
2019-03-19 12:01:22 -0700

    Add a template form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Navigation from './Navigation';
+import TextingForm from './TextingForm';
 import './App.css';
 
 
@@ -8,6 +9,9 @@ class App extends Component {
     return (
       <div className="mainnavbar">
       <Navigation />
+      <div className="formbody">
+      <TextingForm />
+      </div>
       </div>
     );
   }
