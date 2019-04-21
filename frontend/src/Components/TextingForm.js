bcc3ff8769fc51fd83744bfae2135c9789971d78
Princeton <42463391+pwong15@users.noreply.github.com>
2019-04-21 11:47:31 -0700

    Switched back to h2 database for testing and added UserDetailsService

--- frontend/src/Components/TextingForm.js
+++ frontend/src/Components/TextingForm.js
@@ -41,7 +41,7 @@ export default class TextingForm extends Component{
         event.preventDefault();
         const {item, show} = this.state;
 
-        await fetch('/', {
+        await fetch('http://localhost:8080/', {
             method: 'PUT',
             headers: {
                 'Accept': 'application/json',
