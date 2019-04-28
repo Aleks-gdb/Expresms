0f3eec383ee692cf76d4313fd40a559fe4eb9cd4
Aleks <jokerka17@gmail.com>
2019-04-27 18:07:02 -0700

    Some improvements

--- frontend/src/Components/Profile.js
+++ frontend/src/Components/Profile.js
@@ -1,48 +1,66 @@
 import LoggedInNav from "./LoggedInNavbar";
-import React, { Component, Sonnet } from 'react';
-import ListGroup from 'react-bootstrap/ListGroup';
-import {Tab, Row, Col, Nav} from 'react-bootstrap';
-import Messages from './MessageList';
-import Numbers from './NumberList';
+import React, { Component } from 'react';
+// import ListGroup from 'react-bootstrap/ListGroup';
+// import {Tab, Row, Col, Nav} from 'react-bootstrap';
+// import Messages from './MessageList';
+// import Numbers from './NumberList';
 //https://scrimba.com/p/pbNpTv/cm2a6f9
 //https://stackoverflow.com/questions/31234500/create-react-component-dynamically
-/*var NumberList = React.createClass({
-    render: function(){
-        var nums = this.props.data["items"].map(function(itemDate){
-            var component = Components[number['itemClass']];
-            return React.createElement(component, {
-                data: itemData,
-                key: number['id']
-            });
-        });
-    }
-}); */
+
 export default class Profile extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            messages: []
+        }
+    }
+    
+    componentDidMount(){
+        fetch('http://localhost:3000/messages', {
+            method: 'GET'
+        })
+            .then(response => response.json())
+            .then(data => this.setState({messages: data}));
+    }
+
+    handleShow(){
+        return(
+            <table className="table table-hover">
+            <thead>
+                <tr>
+                    <th>Number</th>
+                    <th>Language</th>
+                    <th>Original text</th>
+                    <th>Translated text</th>
+                    <th>Date</th>
+                </tr>
+            </thead>
+            <tbody>
+            {
+                this.state.messages.map(msg =>
+                <tr>
+                <td>{msg.number}</td>
+                <td>{msg.language}</td>
+                <td>{msg.text}</td>
+                <td>{msg.translatedText}</td>
+                <td>{msg.timeStamp}</td>
+                </tr>)
+            }
+            </tbody>
+            </table>
+        );
+    }
+
     render(){
         return(
              <div>
              <LoggedInNav/>
              <h1 id="dashboardHeader">Dashboard</h1>
-             <div id="numberList">
-             <Tab.Container id="left-tabs-example" defaultActiveKey="#link1">
-                <Row>
-                    <Col sm={2}>
-                        <Numbers/>
-                    </Col>
-                    <Col sm={8}>
-                    <Tab.Content>
-                        <Tab.Pane eventKey="number">
-
-                        </Tab.Pane>
-                        <Tab.Pane eventKey="number2">
-                        <Messages/>
-                        </Tab.Pane>
-                    </Tab.Content>
-                    </Col>
-                </Row>
-                </Tab.Container>
-                </div>
-            </div>
+             <div id="messageList">
+             
+                {this.state.messages.length > 0 ? (this.handleShow) : <h2>You have no messages sent!</h2>}
+             </div>
+             </div>
         );
     }
 }
